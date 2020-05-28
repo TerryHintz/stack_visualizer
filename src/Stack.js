76081d3ba118294cc4eb7c00d9df0c8d892d0373
TerryHintz <cza82@sfu.ca>
2020-05-28 12:53:21 -0700

    set up stack display

--- src/Stack.js
+++ src/Stack.js
@@ -2,9 +2,38 @@ import React, {Component} from 'react';
 import './Stack.css'
 
 class Stack extends Component {
+    state = {
+        stack: [],
+    }
+
+    componentDidMount() {
+        let arr = [];
+        for(let i=0; i<10; i++){
+            arr.push(null);
+        }
+        this.setState({stack: arr});
+    }
+
+    push = (val) => {
+        
+    }
+
     render() {
         return(
-            <div>stac</div>
+            <div>
+                <div>
+                    <span>push 5</span>
+                </div>
+                <div className='stack-container'>
+                    {this.state.stack.map((num) => {
+                        return (
+                            <div className='stack-element'>
+                                {num ? num : ''}
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         )
     }
 }
