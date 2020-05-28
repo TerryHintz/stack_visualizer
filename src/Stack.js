8f5555d118596e933574b2002ef91941b094c1a4
TerryHintz <cza82@sfu.ca>
2020-05-28 12:27:13 -0700

    setup stack file

---
+++ src/Stack.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Stack.css'
+
+class Stack extends Component {
+    render() {
+        return(
+            <div>stac</div>
+        )
+    }
+}
+
+export default Stack;
