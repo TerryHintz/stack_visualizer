84781ef0f19d391a2e63202cadc298933c2e71bd
TerryHintz <cza82@sfu.ca>
2020-05-28 15:54:37 -0700

    push implemented

--- src/Stack.js
+++ src/Stack.js
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
 import './Stack.css'
 
+const funcs = [
+    'Push',
+    'Pop',
+    'Peek',
+    'isEmpty',
+]
+
 class Stack extends Component {
     state = {
         stack: [],
+        index: 9,
     }
 
     componentDidMount() {
@@ -14,21 +22,40 @@ class Stack extends Component {
         this.setState({stack: arr});
     }
 
+    handleButton = (func) => {
+        switch(func){
+            case 'Push':
+                this.push(5);
+                break;
+        }
+    }
+
     push = (val) => {
-        
+        const i = this.state.index;
+        const stack = document.getElementsByClassName('stack-element');
+        stack[i].style.backgroundColor = '#ffcc66';
+        setTimeout(() => stack[i].innerHTML = val, 1000);
+        setTimeout(() => stack[i].style.backgroundColor = 'YellowGreen', 1000);
+        this.setState({index: this.state.index - 1});
     }
 
     render() {
         return(
             <div>
                 <div>
-                    <span>push 5</span>
+                    {funcs.map((func) => {
+                        return (
+                            <div>
+                                <span onClick={() => this.handleButton(func)}>{func}</span>
+                            </div>
+                        )
+                    })}
                 </div>
                 <div className='stack-container'>
-                    {this.state.stack.map((num) => {
+                    {this.state.stack.map((num, index) => {
                         return (
-                            <div className='stack-element'>
-                                {num ? num : ''}
+                            <div key={index} className='stack-element'>
+                                {''}
                             </div>
                         )
                     })}
